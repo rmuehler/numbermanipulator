af7b49e0dfd1ca17c8149611a81ebfeaab0eadbf
Robert Muehler <robertmuehler@gmail.com>
2021-05-10 16:45:14 -0400

    finished assignment

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace numbermanipulator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var x = Int32.Parse(Console.ReadLine());
+            x *= x;
+            x += 14;
+            x -= 3;
+            Console.WriteLine(x);
+        }
+    }
+}
